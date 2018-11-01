b0a620baadadeadce9cd94a9ecbdf5f182a19ae9
hnkz <cs16075@s.inf.shizuoka.ac.jp>
2018-11-02 02:33:30 +0900

    fix to line vec

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,55 @@
 
+pub mod pdfreader;
 pub mod header;
 pub mod object;
+
+
+use self::header::Header;
+use self::object::obj::Obj;
+
+#[derive(Debug)]
+pub struct PDF {
+    header: Header,
+    obj: Vec<Obj>,
+}
+
+impl PDF {
+    pub fn new(buf: Vec<u8>) -> Result<PDF, String> {
+        let mut buf = PDF::get_line(buf);
+        let header = match Header::check_header(&buf[0]) {
+            Ok(header) => {
+                buf.remove(0);   
+                header
+            }
+            Err(err) => return Err(err)
+        };
+
+        let mut obj: Vec<Obj> = Vec::new();
+        for line in buf {
+            
+        }
+
+
+        Ok(PDF {
+            header: header,
+            obj: obj
+        })
+    }
+
+    fn get_line(buf: Vec<u8>) -> Vec<Vec<u8>> {
+        let mut res: Vec<Vec<u8>> = Vec::new();
+        let mut line: Vec<u8> = Vec::new();
+        for b in buf {
+            // '0x0a'
+            if b == 10 {
+                // println!("{:x?}", line);
+                res.push(line.clone());
+                line.clear();
+            } else {
+                line.push(b);
+            }
+        }
+
+        res
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
-extern crate pdflib;
+#![feature(dbg_macro)]
 
+extern crate pdflib;
+use pdflib::PDF;
+use std::{fs};
+use std::io::{
+    BufReader, Read,
+};
 
 fn main() {
+    let mut buf: Vec<u8> = Vec::new();
+    let mut reader = BufReader::new(fs::File::open("pdf/PRML1-20.pdf").expect("File does not exist."));
 
+    reader.read_to_end(&mut buf).expect("read_to_end error");
+
+    let pdf = match PDF::new(buf) {
+        Ok(pdf) => pdf,
+        Err(err) => {
+            println!("pdf new error: {}", err);
+            panic!(); 
+        }
+    };
+
+    dbg!(&pdf);
 }
